5f373ce5a65dc39ec20315988a2a7f6a39608463
Asuan <sngasuan@gmail.com>
2018-10-08 08:49:19 +0300

    Make Market Optional filed as optional

--- service_account.go
+++ service_account.go
@@ -29,9 +29,13 @@ func (as *apiService) NewOrder(or NewOrderRequest) (*ProcessedOrder, error) {
 	params["symbol"] = or.Symbol
 	params["side"] = string(or.Side)
 	params["type"] = string(or.Type)
-	params["timeInForce"] = string(or.TimeInForce)
+	if len(or.TimeInForce) > 0 {
+		params["timeInForce"] = string(or.TimeInForce)
+	}
 	params["quantity"] = strconv.FormatFloat(or.Quantity, 'f', -1, 64)
-	params["price"] = strconv.FormatFloat(or.Price, 'f', -1, 64)
+	if or.Price > 0 {
+		params["price"] = strconv.FormatFloat(or.Price, 'f', -1, 64)
+	}
 	params["timestamp"] = strconv.FormatInt(unixMillis(or.Timestamp), 10)
 	if or.NewClientOrderID != "" {
 		params["newClientOrderId"] = or.NewClientOrderID
